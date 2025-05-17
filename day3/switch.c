767809785859732f107ed2a5760b512fc171bbf4
Lithessh123 <vijayramyalithessh@gmail.com>
2025-05-17 11:45:26 +0530

    Add files via upload

---
+++ day3/switch.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int x=45;
+    if(0.1)
+        printf("a");
+    }
+    if(x>=50){
+        printf("s");
+    }
+    else{printf("t");}
+    else if(x>=30){
+        printf("b");
+    }
+    else{
+        printf("c");
+    }
+    return 0;
